04f9a6e118e31fa934ab8ebc1eb1bfe0efc33d59
Olle Bergkvist <olle.bergkvist.devserver@nouw.com>
2018-11-07 18:36:09 +0100

    Add subproject and source files from ACMESharp.Providers.IIS

---
+++ ACMESharp.Providers.IIS/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ACMESharp IIS Provider")]
+[assembly: AssemblyDescription("Microsoft IIS Provider extension for ACMESharp")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("github.com/ebekker/ACMESharp")]
+[assembly: AssemblyProduct("ACMESharp.Providers.IIS")]
+[assembly: AssemblyCopyright("Copyright © " + ASMINFO.COPYRIGHT_YEAR + " Eugene Bekker. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("944ec9bd-81a1-484e-ba29-360bc155bf51")]
